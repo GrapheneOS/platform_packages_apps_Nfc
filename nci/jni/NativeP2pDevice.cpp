a66137587bf8c88608aeb5f0aaf30df480c63145
Daniel Micay <danielmicay@gmail.com>
2019-03-17 20:58:37 -0400

    constify JNINativeMethod tables

--- nci/jni/NativeP2pDevice.cpp
+++ nci/jni/NativeP2pDevice.cpp
@@ -58,7 +58,7 @@ static jboolean nativeP2pDeviceDoSend(JNIEnv*, jobject, jbyteArray) {
 ** Description:     JNI functions
 **
 *****************************************************************************/
-static JNINativeMethod gMethods[] = {
+static const JNINativeMethod gMethods[] = {
     {"doConnect", "()Z", (void*)nativeP2pDeviceDoConnect},
     {"doDisconnect", "()Z", (void*)nativeP2pDeviceDoDisconnect},
     {"doTransceive", "([B)[B", (void*)nativeP2pDeviceDoTransceive},
